551aad93c58ce43361e5e8708b0ee57a61cb8d6d
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-03-27 22:01:36 -0600

    better order
    '''
    $ ./normalize_batch.R -h
    usage: ./normalize_batch.R [-h] --matrix MATRIX cel [cel ...]
    
    Normalize CEL files of a same batch.
    
    positional arguments:
      cel              CEL files to normalize
    
    optional arguments:
      -h, --help       show this help message and exit
        --matrix MATRIX  normalized expression matrix
    '''

--- normalize_batch.R
+++ normalize_batch.R
@@ -1,17 +1,19 @@
 #!/usr/bin/env Rscript
+#
+#
+# try $ ./normalize_batch.R -h for help
+#
+#
+
 suppressPackageStartupMessages(library(argparse))
-suppressPackageStartupMessages(library(affy))
-suppressPackageStartupMessages(library(limma,gplots))
-
-#library(GO.db)
-#library(annotate)
-
 
 parser <- ArgumentParser(description="Normalize CEL files of a same batch.")
 parser$add_argument("--matrix", required=TRUE, help="normalized expression matrix")
 parser$add_argument("cel", nargs="+", help="CEL files to normalize")
 args   <- parser$parse_args()
 
+suppressPackageStartupMessages(library(affy))
+suppressPackageStartupMessages(library(limma,gplots))
 
 # read 'em
 affybatch <- read.affybatch(filenames=args$cel)
