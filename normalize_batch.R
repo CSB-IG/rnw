37f45ffe02ca1bc837c491e22900802aa7edf48f
Rodrigo Garcia <rgarcia@inmegen.gob.mx>
2015-03-27 22:34:04 -0600

    bg correct, normalize, per #3
    usage: ./normalize_batch.R [-h]
                               [--bgcorrect {bg.correct,mas,none,rma}]
                               [--normalize {constant,contrasts,invariantset,loess,methods,qspline,quantiles,quantiles.robust}]
                               --matrix MATRIX
                               cel [cel ...]

--- normalize_batch.R
+++ normalize_batch.R
@@ -6,20 +6,26 @@
 #
 
 suppressPackageStartupMessages(library(argparse))
+suppressPackageStartupMessages(library(affy))
 
 parser <- ArgumentParser(description="Normalize CEL files of a same batch.")
+parser$add_argument("--bgcorrect", default="none",      choices=bgcorrect.methods(), help="method for background noise correction")
+parser$add_argument("--normalize", default="quantiles", choices=normalize.AffyBatch.methods(), help="normalizing method")
 parser$add_argument("--matrix", required=TRUE, help="normalized expression matrix")
 parser$add_argument("cel", nargs="+", help="CEL files to normalize")
 args   <- parser$parse_args()
 
-suppressPackageStartupMessages(library(affy))
+
 suppressPackageStartupMessages(library(limma,gplots))
 
 # read 'em
 affybatch <- read.affybatch(filenames=args$cel)
 
 # normalize 'em
-eset <- expresso(affybatch, bgcorrect.method="none", normalize.method="quantiles", pmcorrect.method="pmonly", summary.method="medianpolish")
+eset <- expresso(affybatch, bgcorrect.method=args$bgcorrect,
+                            normalize.method=args$normalize,
+                            pmcorrect.method="pmonly",
+                            summary.method="medianpolish")
 
 # write 'em!
 write.csv(eset, file=args$matrix)
