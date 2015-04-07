feaf5b93a21bdeb890e78507010112226b9297b2
hachepunto <hachepunto@gmail.com>
2015-04-07 12:43:41 -0500

    Correcting the output of the matrix expression (adding line 29). Adding normalization report generator. ref #3

--- normalize_batch.R
+++ normalize_batch.R
@@ -22,10 +22,26 @@ suppressPackageStartupMessages(library(limma,gplots))
 affybatch <- read.affybatch(filenames=args$cel)
 
 # normalize 'em
-eset <- expresso(affybatch, bgcorrect.method=args$bgcorrect,
+eset.quantiles <- expresso(affybatch, bgcorrect.method=args$bgcorrect,
                             normalize.method=args$normalize,
                             pmcorrect.method="pmonly",
                             summary.method="medianpolish")
-
+eset<-exprs(eset.quantiles)
 # write 'em!
 write.csv(eset, file=args$matrix)
+
+# report 'em
+report = file.path(dirname(args$matrix), paste(basename(args$matrix),".pdf",sep=""))
+pdf(file=report,width=7,height=5)
+N=length(affybatch@phenoData@data$sample)
+pm.mm=0
+for (i in 1:N) {pm.mm[i] = mean(mm(affybatch[,i])>pm(affybatch[,i]))}
+mycolors = rep(c("blue","red","green", "magenta"), each = 2)
+hist(affybatch, col=mycolors, main="Raw data distribution")
+boxplot(affybatch,col=mycolors, main="Raw data distribution")
+plot(100*pm.mm, type='h', main='Percent of MMs > PMs', ylab="%",xlab="Microarrays", ylim=c(0,50), col="red", lwd=5 )
+grid(nx = NULL, ny = 6, col = "blue", lty = "dotted",lwd = par("lwd"), equilogs = TRUE)
+mycolors = rep(c("blue","red","green", "magenta"), each = 2)
+plotDensity(eset, col=mycolors, main="Data After normalization")
+boxplot(eset,col=mycolors, main="Data After normalization")
+dev.off()
