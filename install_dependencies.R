f6c048366c2fa032dcf5c6599d0ed3322c36ace7
Aldo <ajosuehv@gmail.com>
2015-06-03 16:01:11 -0500

    Added Dependencies used with Bioconductor

--- install_dependencies.R
+++ install_dependencies.R
@@ -2,3 +2,13 @@
 install.packages("argparse")
 library(devtools)
 install_github("ggbiplot", "vqv")
+
+# Install with Bioconductor
+#ctc
+#heatmap.plus
+#impute
+#genefu
+#limma
+#affy
+#annotate
+#hugene10sttranscriptcluster.db
