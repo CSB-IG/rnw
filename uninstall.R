9e3b12a673433aa3624bfe696111e68972399d46
Angel García-Campos <xxwildfangxx@gmail.com>
2015-06-23 14:06:32 -0500

    Create uninstall.R
    An script to uninstall all packages/bundles and updates obtained by running "install_dependencies.R"

---
+++ uninstall.R
@@ -0,0 +1,14 @@
+## Removing all packages/bundles and updates for running RNW scripts
+
+remove.packages("argparse")
+remove.packages("devtools")
+remove.packages("ggbiplot")
+# remove.packages("vqv") #package not found!!
+remove.packages("ctc")
+remove.packages("heatmap.plus")
+remove.packages("impute")
+remove.packages("genefu")
+remove.packages("limma")
+remove.packages("affy")
+remove.packages("annotate")
+remove.packages("hugene10sttranscriptcluster.db")
