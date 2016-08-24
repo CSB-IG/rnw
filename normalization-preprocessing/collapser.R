6d7aed8f86d2bb7762491ca80268e6f340e35dcd
dianadrago <didra3@gmail.com>
2016-08-23 22:25:04 -0500

    update collapser add sorting edata in pheno order

--- normalization-preprocessing/collapser.R
+++ normalization-preprocessing/collapser.R
@@ -22,6 +22,9 @@ args <- parser$parse_args()
 edata <- as.matrix(read.table(file=args$matrix, header=TRUE, sep="\t", row.names=1))
 pheno <- read.table(file=args$phenotype, header = TRUE, sep = "\t", row.names=1)
 
+# pheno samples and edata column names (samples) must be in the same order
+edata <- edata[ ,(as.vector(pheno$sample_name))]
+
 # annotation
 gconvert <- gconvert(query=rownames(edata), target="HGNC", mthreshold=1, filter_na=FALSE)
 class(gconvert[,1])<-"integer"
